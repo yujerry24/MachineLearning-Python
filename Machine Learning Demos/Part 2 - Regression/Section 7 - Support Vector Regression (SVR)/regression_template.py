d0d02007d68031e3bcd8b7de645adce6cb69a25f
Jerry Yu <yujerry24@gmail.com>
2018-08-22 13:04:10 -0400

    SVR

--- Machine Learning Demos/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/regression_template.py
+++ Machine Learning Demos/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/regression_template.py
@@ -15,24 +15,23 @@ y = dataset.iloc[:, 2].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
+X=sc_X.fit_transform(X)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+y=sc_y.fit_transform(y)
 
 # Fitting the Regression Model to the dataset
 # Create your regressor here
 
 # Predicting a new result
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
+from sklearn.svm import SVR
+regressor= SVR( kernel ='rbf')
 regressor.fit(X,y)
 y_pred = regressor.predict(6.5)
 
 # Visualising the Regression results
-plt.scatter(X, y, color = 'red') 
+plt.scatter(X, y, color = 'red')
 plt.plot(X, regressor.predict(X), color = 'blue')
 plt.title('Truth or Bluff (Regression Model)')
 plt.xlabel('Position level')
