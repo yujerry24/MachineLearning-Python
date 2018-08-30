1e574e8ef039b7b2698d23c556900e156dc4ff48
Jerry Yu <yujerry24@gmail.com>
2018-08-30 13:20:39 -0400

    Attempt to inverse transform the listedcolormap

--- Machine Learning Demos/Part 3 - Classification/Section 14 - Logistic Regression/logistic_regression.py
+++ Machine Learning Demos/Part 3 - Classification/Section 14 - Logistic Regression/logistic_regression.py
@@ -35,6 +35,7 @@ cm = confusion_matrix(y_test, y_pred)
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
+"X_set = sc.inverse_transform(X_train)"
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
